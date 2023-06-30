7470bef39382b2af8e8bf369320a89121195e9b5
jaswantikantumuchu <jaswanti.kantumuchu@eagles.oc.edu>
2023-06-30 11:01:20 -0500

    Employee screen implementation

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,11 @@ const router = createRouter({
       name: "login",
       component: () => import("../views/Login.vue"),
     },
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/AboutView.vue"),
-    // },
+    {
+      path: "/Employees",
+      name: "employees",
+      component: () => import("../views/Employees.vue"),
+    },
   ],
 });
 
